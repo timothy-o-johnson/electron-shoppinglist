4cd00d3ed7fa7df32b1143b6cfc9a7dc09102078
Timothy O Johnson <pavementii@gmail.com>
2018-04-04 10:38:43 -0400

    connected new item to main window

--- shoppinglist/main.js
+++ shoppinglist/main.js
@@ -2,7 +2,7 @@ const electron = require('electron')
 const url = require('url')
 const path = require('path')
 
-const { app, BrowserWindow, Menu } = electron
+const { app, BrowserWindow, Menu, ipcMain } = electron
 
 let mainWindow
 let addWindow
@@ -55,6 +55,14 @@ function createAddWindow () {
     addWindow = null
   })
 }
+
+// catch item:add
+ipcMain.on('item:add', function(e, item){
+  console.log(item);
+
+  mainWindow.webContents.send('item:add', item)
+  addWindow.close()
+})
 
 // create menu template
 const mainMenuTemplate = [
